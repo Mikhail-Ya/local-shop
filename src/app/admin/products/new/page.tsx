dc8057aff1f69a533b5da44c291eb3780caefa38
Mikhail-Ya <deff696@gmail.com>
2026-01-24 17:25:55 +0300

    build version 1.0.2

--- src/app/admin/products/new/page.tsx
+++ src/app/admin/products/new/page.tsx
@@ -4,6 +4,8 @@ import { prisma } from '@/lib/prisma';
 import ProductForm from '@/components/admin/ProductForm';
 import { CategoryOption } from '@/types/product';
 
+export const dynamic = 'force-dynamic';
+
 export default async function NewProductPage() {
   const categories = await prisma.category.findMany();
 
